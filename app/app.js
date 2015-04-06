6c500eda525f9f40be7460dd2a87a47d33ffba2a
chujun2018 <chujun1990@hotmail.com>
2015-04-06 01:00:48 +0100

    second commit

--- app/app.js
+++ app/app.js
@@ -16,33 +16,35 @@ config(['$routeProvider', function($routeProvider, $rootScope, $scope) {
     var englishLang = {
       title : 'Wedding Photography',
       home : 'Home',
-      phtographer : 'Gallary',
-      contact : 'Contact',
+      phtographer : 'Wedding Photographers',
+      contact : 'Customer Service',
       search : 'Search',
       uk : 'UK',
       fr : 'France',
-      sw : 'Sweden',
+      it : 'Italy',
       usa : 'America',
       jp : 'Japan',
       kr : 'Korea',
       previous :'previous',
-      next:'next'
+      next:'next',
+      getInTouch:'Contact Photographers'
     };
 
       var chineseLang = {
       title : '海外婚纱摄影',
         home : '主页',
-        phtographer : '婚纱摄影',
-        contact : '联系方式',
+        phtographer : '联系摄影师',
+        contact : '客户服务',
         search : '搜索',
         uk : '英国',
         fr : '法国',
-        sw : '瑞典',
+        it : '意大利',
         usa : '美国',
         jp : '日本',
         kr : '韩国',
               previous :'上一张',
-              next:'下一张'
+              next:'下一张',
+              getInTouch:'联系摄影师'
       };
      $rootScope.language = {};
     $rootScope.language = chineseLang;
@@ -52,4 +54,7 @@ config(['$routeProvider', function($routeProvider, $rootScope, $scope) {
       $rootScope.switch2Chinese = function(){
           $rootScope.language = chineseLang;
       };
+
+      $rootScope.mainActive = 'active';
+
 }]);
