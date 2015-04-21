b8201ec822774badbb5f0f33f0a7d13daa0c3ada
chujun2018 <chujun1990@hotmail.com>
2015-04-22 00:32:11 +0100

    home page almost done

--- app/home/home.js
+++ app/home/home.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.home', ['ngRoute'])
+angular.module('myApp.home', ['ngRoute', 'ui.bootstrap.datetimepicker'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/home', {
